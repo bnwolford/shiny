f1daa695e2d6854c27e23d3e22295b431029c566
Brooke Wolford <bwolford@umich.edu>
2020-02-24 00:10:48 -0500

    global

--- troponin/global.R
+++ troponin/global.R
@@ -5,3 +5,9 @@ library(data.table)
 library(tidyverse)
 library(DT)
 library(ggvis)
+
+
+A<-fread("troponin_variants_HRC_lookup_phenoDefs.txt.gz")
+B<-fread("troponin_variants_HRC_lookup_phenoDefs.txt.gz")
+data_sets<-list(A,B)
+
