8995549be3545a2b9f1b56e9eeab3ec666b5d525
Brooke Wolford <bwolford@umich.edu>
2020-02-24 00:10:57 -0500

    add a build functionality

--- troponin/ui.R
+++ troponin/ui.R
@@ -1,10 +1,15 @@
 
 ### Define UI logic
 fluidPage(
-
-    titlePanel("Phecodes in UKBB"),
+    titlePanel("Troponin Top Variant PheWAS + Quant Traits in UKBB"),
     tableOutput("data"),
     
+    sidebarPanel(
+      selectizeInput('dataset', 'Select dataset', choices = c("choose" = "", c("Quantitative","Phecode"))),
+      actionButton(inputId = "go",label = "Build!")
+      ),
+    
+
     mainPanel(
         ggvisOutput("plot2"),
         DT::dataTableOutput("mytable1")
